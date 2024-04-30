ef0ff78fde7efb327245d590ea08a3411919f6a8
Chralu <contact@charly-caulet.net>
2024-04-30 16:14:07 +0200

    ⚡️ Remove sourcemap from js prod build.

---
+++ components/webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+module.exports = {
+    mode: "production",
+    entry: {
+        youtube_playlist: path.resolve(__dirname, "youtube-playlist", "youtube-playlist.ts"),
+    },
+    output: {
+        library: "[name]",
+        path: path.join(__dirname, "../public/assets/js"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+};
